71b1c1b45bdbdd4a535204508f319759a741f1a1
David Pavlovski <david.pavlovski@hotmail.com>
2022-03-15 21:50:38 +0100

    Homework_04

---
+++ Homework_04/StudentGroup/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace StudentGroup
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studentsG1 = new string[5];
+            string[] studentsG2 = new string[5];
+
+            Console.WriteLine("Enter the names of the students in group G1.");
+            for (int i = 0; i < studentsG1.Length; i++)
+            {
+                Console.Write($"Enter student number {i + 1} : ");
+                string studentName = Console.ReadLine();
+                studentsG1[i] = studentName;
+            }
+
+            Console.WriteLine("\nEnter the names of the students in group G2.");
+            for (int i = 0; i < studentsG2.Length; i++)
+            {
+                Console.Write($"Enter student number {i + 1} : ");
+                string studentName = Console.ReadLine();
+                studentsG2[i] = studentName;
+            }
+
+            while (true)
+            {
+                Console.WriteLine("Print student list.");
+                Console.Write("Enter group number to print its students (1.G1 , 2.G2): ");
+                string groupNum = Console.ReadLine();
+
+                if (groupNum == "1")
+                {
+                    while (true)
+                    {
+                        Console.Write("Would you like to insert another student (y/any) ?");
+                        string input = Console.ReadLine();
+                        if (input.ToLower() != "y")
+                        {
+                            Console.WriteLine("G1 students :");
+                            for (int i = 0; i < studentsG1.Length; i++)
+                            {
+                                Console.WriteLine($"{i + 1}.{studentsG1[i]}");
+                            }
+                            break;
+                        }
+                        Console.Write($"Enter student number {studentsG1.Length + 1} : ");
+                        string studentName = Console.ReadLine();
+                        Array.Resize(ref studentsG1, studentsG1.Length + 1);
+                        studentsG1[studentsG1.Length - 1] = studentName;
+                        continue;
+                    }
+                    break;
+                }
+
+                else if (groupNum == "2")
+                {
+                    while (true)
+                    {
+                        Console.Write("Would you like to insert another student (y/any) ?");
+                        string input = Console.ReadLine();
+                        if (input.ToLower() != "y")
+                        {
+                            Console.WriteLine("G2 students :");
+                            for (int i = 0; i < studentsG2.Length; i++)
+                            {
+                                Console.WriteLine($"{i + 1}.{studentsG2[i]}");
+                            }
+                            break;
+                        }
+                        Console.Write($"Enter student number {studentsG2.Length + 1} : ");
+                        string studentName = Console.ReadLine();
+                        Array.Resize(ref studentsG2, studentsG2.Length + 1);
+                        studentsG2[studentsG2.Length - 1] = studentName;
+                        continue;
+                    }
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("There is not a group with that number");
+                    continue;
+                }
+            }
+        }
+    }
+}
---
+++ Homework_04/SumOfEven/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace SumOfEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[6];
+            int oddSum = 0;
+            int evenSum = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                while (true)
+                {
+                    Console.Write($"Enter number {i + 1} : ");
+                    string input = Console.ReadLine();
+                    if (!int.TryParse(input, out int number))
+                    {
+                        Console.WriteLine($"{input} is not a valid integer. Please try again.");
+                        continue;
+                    }
+                    numbers[i] = number;
+
+                    break;
+                }
+            }
+
+            while (true)
+            {
+                Console.Write("Would you like to an additional number (y / any)? ");
+                string addMore = Console.ReadLine();
+                if (addMore != "y" && addMore != "Y") // ->  if (addMore !="y" && addmore != "Y")
+                {
+                    Console.WriteLine("Calculating results");
+                    break;
+                }
+
+                while (true)
+                {
+                    Console.Write($"Enter number {numbers.Length + 1} : ");
+                    string input = Console.ReadLine();
+                    if (!int.TryParse(input, out int number))
+                    {
+                        Console.WriteLine($"{input} is not a valid integer. Please try again.");
+                        continue;
+                    }
+                    Array.Resize(ref numbers, numbers.Length + 1);
+                    numbers[numbers.Length - 1] = number;
+                    break;
+                }
+            }
+
+            Console.WriteLine("You have entered : ");
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    evenSum += number;
+                }
+                else
+                {
+                    oddSum += number;
+                }
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine($"\nThe sum of odd numbers is : {oddSum}");
+            Console.WriteLine($"The sum of even numbers is : {evenSum}");
+        }
+    }
+}
